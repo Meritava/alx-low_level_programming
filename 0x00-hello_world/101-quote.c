8d075f033145e1ffa3f5f0fd51fb59a714d96203
Meritava <georgemeritava@gmail.com>
2023-05-20 05:50:58 -0700

    n

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 
 int main()
 {
-	fprint(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprint(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return(1);
 }
