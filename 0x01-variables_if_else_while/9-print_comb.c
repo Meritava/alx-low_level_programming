6130bdf3936d125db6f32106773cbf7e1bb8a737
Meritava <georgemeritava@gmail.com>
2023-05-22 14:50:51 -0700

    l

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void);
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
