ad84afb5d09008b5ea4a6cb799b5a8d90081fff9
Meritava <georgemeritava@gmail.com>
2023-05-23 07:18:05 -0700

    prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int _putchar(char c)
+{
+	char c = '_putchar';
+
+	putchar(c);
+	return (0);
+}
